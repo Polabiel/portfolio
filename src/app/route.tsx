1037a7ce5651a230c233caef0d0f47fc5b0a3eb2
Gabriel Oliveira <bielgabrieloliveira77@gmail.com>
2023-08-11 14:02:12 -0300

    fix: 😎 Problema de moduleResolution

--- src/app/route.tsx
+++ src/app/route.tsx
@@ -1,5 +1,10 @@
 import { NextResponse } from 'next/server'
 
-export default function GET() {
-  return new NextResponse(JSON.stringify({ message: 'world'}))
-}
+export async function GET() {
+
+  const repo = await fetch('https://api.github.com/users/polabiel')
+
+  const data = await repo.json()
+
+  return new NextResponse(data.avatar_url)
+};
