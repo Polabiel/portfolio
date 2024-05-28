4cb03ee86673a7763b6a0d80ff6d543162a3541d
Gabriel Oliveira <40695127+Polabiel@users.noreply.github.com>
2024-05-28 16:50:11 -0300

    chore: Update dependencies and add @headlessui/react package

--- src/components/MenuRepository.tsx
+++ src/components/MenuRepository.tsx
@@ -1,22 +1,38 @@
 import Link from "next/link";
 
 export default function() {
+
+    const menuItems = [
+        { name: 'Sobre Mim', href: '/' },
+        { name: 'Projetos', href: '/projects' },
+        { name: 'Contatos', href: '/contact' },
+    ];
+
     return (
-        <nav>
-            <div className="logo">
-                <h1>Gabriel Olveira</h1>
+        <nav className="bg-blue-700 shadow-md">
+            <div className="container mx-auto px-4 py-6 flex items-center justify-between">
+                <div className="flex items-center">
+                    <Link href="/" className="text-xl font-bold text-gray-800">Gabriel Oliveira</Link>
+                </div>
+
+                <ul className={`hidden ${false ? 'block' : 'lg:flex'}`}>
+                    {menuItems.map((item) => (
+                        <li key={item.name} className="mt-4 lg:mt-0">
+                            <a
+                                href={item.href}
+                                className={[
+                                    'text-gray-100 hover:text-gray-300',
+                                    'font-semibold text-sm',
+                                    'transition duration-300',
+                                    'ease-in-out',
+                                ].join(' ')}
+                            >
+                                {item.name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
             </div>
-            <ul>
-                <li>
-                    <Link href={"about"}>Sobre mim</Link>
-                </li>
-                <li>
-                    <Link href={"projects"}>Projetos</Link>
-                </li>
-                <li>
-                    <Link href={"Contacts"}>Contatos</Link>
-                </li>
-            </ul>
         </nav>
-    )
-}
+    );
+};
